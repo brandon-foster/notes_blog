13ab51e442052d8055d897a56a83a956243805fd
Brandon-Foster <brandon.foster.vt@gmail.com>
2014-01-28 03:51:29 -0500

    change content from TextField to RedactorField

--- blog/models.py
+++ blog/models.py
@@ -7,7 +7,7 @@ class Post(models.Model):
     title = models.CharField(max_length=255)
     slug = models.SlugField(unique=True, max_length=255)
     description = models.CharField(max_length=255)
-    content = models.TextField()
+    content = RedactorField(verbose_name=u'Text')
     published = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True)
     
