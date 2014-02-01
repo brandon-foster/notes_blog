c2e343eadd3e11b0c52fcd5f75619b5fa223fc76
Brandon-Foster <brandon.foster.vt@gmail.com>
2014-02-01 01:36:33 -0500

    push south migration 0002

---
+++ blog/migrations/0002_auto__add_field_post_course_name__chg_field_post_content.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+from south.utils import datetime_utils as datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding field 'Post.course_name'
+        db.add_column(u'blog_post', 'course_name',
+                      self.gf('django.db.models.fields.CharField')(default='course name', max_length=255),
+                      keep_default=False)
+
+
+        # Changing field 'Post.content'
+        db.alter_column(u'blog_post', 'content', self.gf('redactor.fields.RedactorField')())
+
+    def backwards(self, orm):
+        # Deleting field 'Post.course_name'
+        db.delete_column(u'blog_post', 'course_name')
+
+
+        # Changing field 'Post.content'
+        db.alter_column(u'blog_post', 'content', self.gf('django.db.models.fields.TextField')())
+
+    models = {
+        u'blog.post': {
+            'Meta': {'ordering': "['-created']", 'object_name': 'Post'},
+            'content': ('redactor.fields.RedactorField', [], {}),
+            'course_name': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'created': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'description': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'published': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'slug': ('django.db.models.fields.SlugField', [], {'unique': 'True', 'max_length': '255'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '255'})
+        }
+    }
+
+    complete_apps = ['blog']
