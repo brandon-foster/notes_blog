ea38a7c67b3905563f7ea0be79cc1ae650544f70
Brandon-Foster <brandon.foster.vt@gmail.com>
2014-02-01 14:05:35 -0500

    wrap the WSGI application with newrelic

--- notes_blog/wsgi.py
+++ notes_blog/wsgi.py
@@ -8,9 +8,12 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
 import os
+import newrelic.agent
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "notes_blog.settings.prod")
 
 from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
 
 application = Cling(get_wsgi_application())
+application = newrelic.agent.WSGIApplicationWrapper(application)
