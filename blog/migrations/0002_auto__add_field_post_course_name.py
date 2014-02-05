ec622f67a4995034cc0d4fa60b9abffb77b4b9d1
Brandon-Foster <brandon.foster.vt@gmail.com>
2014-02-05 02:23:53 -0500

    adapt to use Pure CSS blog layout

---
+++ blog/migrations/0002_auto__add_field_post_course_name.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+from south.utils import datetime_utils as datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding field 'Post.course_name'
+        db.add_column(u'blog_post', 'course_name',
+                      self.gf('django.db.models.fields.CharField')(default='course name', max_length=255),
+                      keep_default=False)
+
+
+    def backwards(self, orm):
+        # Deleting field 'Post.course_name'
+        db.delete_column(u'blog_post', 'course_name')
+
+
+    models = {
+        u'blog.post': {
+            'Meta': {'ordering': "['-created']", 'object_name': 'Post'},
+            'content': ('django.db.models.fields.TextField', [], {}),
+            'course_name': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'created': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'description': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'published': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'slug': ('django.db.models.fields.SlugField', [], {'unique': 'True', 'max_length': '255'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '255'})
+        }
+    }
+
+    complete_apps = ['blog']
